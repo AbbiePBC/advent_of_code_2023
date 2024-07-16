456d3e3a60306204a54adf05dfd648f2f74b6f35
AbbiePBC <71944252+AbbiePBC@users.noreply.github.com>
2024-07-16 11:02:24 +0100

    day 1 improvements
    - since the lines in the input are short, iterating through them twice, from each direction is an unnecessary complication, and we can just find all the matches in one go.
    - using regex is cleaner and allows us to use a precompiled module to help
    - using a dict to get the word's value avoids having to mess around with indexing.

--- one.py
+++ one.py
@@ -1,54 +1,18 @@
 import sys
+import re
 
-def part_one_parse_line(line: str) -> int:
-    for char in line:
-        if char.isnumeric():
-            first_digit = char
-            break
+def parse_line(line: str) -> int:
+    digits = re.findall(r'(?=(one|two|three|four|five|six|seven|eight|nine|[1-9]))', line)
+    values = [str(word_to_digit(d)) if d.isalpha() else d for d in digits]
+    return int(values[0] + values[-1])
 
-    for char in line[::-1]:
-        if char.isnumeric():
-            last_digit = char
-            break
-
-    return int(first_digit)*10 + int(last_digit)
-
-def numeric_word(line: str, idx: int) -> int:
-    words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    for w in range(len(words)):
-        if line[idx:].startswith(words[w]):
-            return w + 1 # 1-indexed
-    return -1
-
-
-def part_two_parse_line(line: str) -> int:
-
-    for i in range(len(line)):
-        if line[i].isnumeric():
-            first_digit = line[i]
-            break
-        digit = numeric_word(line, i)
-        if digit != -1:
-            first_digit = digit
-            break
-
-    for i in range(len(line)-1, -1, -1):
-        if line[i].isnumeric():
-            last_digit = line[i]
-            break
-        digit = numeric_word(line, i)
-        if digit != -1:
-            last_digit = digit
-            break
-
-    return int(first_digit)*10 + int(last_digit)
+def word_to_digit(word: str) -> int:
+    words = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+    return words.get(word, -1)
 
 def parse_input(filename: str):
     with open(filename) as f:
-        lines = f.readlines()
-        return sum(part_two_parse_line(line) for line in lines)
+        return sum(parse_line(line) for line in f.readlines())
 
 if __name__ == "__main__":
-    # Assume program is called correctly
     print(f"Solution: {parse_input(sys.argv[1])}")
-
