68998ee029c0d22152c857afc36f608d460c6de3
AbbiePBC <71944252+AbbiePBC@users.noreply.github.com>
2024-07-19 23:19:23 +0100

    day 5: part 2, java

--- five.java
+++ five.java
@@ -12,7 +12,7 @@ public class five {
 
     public class Almanac {
 
-        private static List<Long> seeds = new ArrayList<Long>();
+        private static List<Long[]> seeds = new ArrayList<Long[]>();
         private static Map<Long, Long[]> seed_to_soil = new HashMap<Long, Long[]>();
         private static Map<Long, Long[]> soil_to_fertilizer = new HashMap<Long, Long[]>();
         private static Map<Long, Long[]> fertilizer_to_water = new HashMap<Long, Long[]>();
@@ -21,7 +21,7 @@ public class five {
         private static Map<Long, Long[]> temperature_to_humidity = new HashMap<Long, Long[]>();
         private static Map<Long, Long[]> humidity_to_location = new HashMap<Long, Long[]>();
 
-        public static long get_value(long source, Map<Long, Long[]> map){
+        private static long get_value(long source, Map<Long, Long[]> map){
             // given a map of seed : soil, range, and a seed to find
             // returns the value of the soil
             if (map.containsKey(source)){
@@ -49,21 +49,32 @@ public class five {
 
         public static long calculate_solution(){
             long solution = Long.MAX_VALUE;
-            for (var seed: Almanac.seeds){
-                var val = get_value(seed, Almanac.seed_to_soil);
-                val = get_value(val, Almanac.soil_to_fertilizer);
-                val = get_value(val, Almanac.fertilizer_to_water);
-                val = get_value(val, Almanac.water_to_light);
-                val = get_value(val, Almanac.light_to_temperature);
-                val = get_value(val, Almanac.temperature_to_humidity);
-                val = get_value(val, Almanac.humidity_to_location);
+            for (var seed_range: Almanac.seeds){
+                var first_seed = seed_range[0];
+                var range = seed_range[1];
+                for (long i = first_seed; i < first_seed + range; i ++){
+                    // TODO: can potentially optimise this by not doing the calculations for every seed
+                    // or reversing the maps, so that instead of going through every seed
+                    // (which was fine in part one, when there were fewer seeds than any other vals)
+                    // we can sort location map and for smallest key,
+                    // find the seed and if it is in the initial input.
+                    // That's more of a rewrite though, so just using brute-force for now.
+                    var val = get_value(i, Almanac.seed_to_soil);
+                    val = get_value(val, Almanac.soil_to_fertilizer);
+                    val = get_value(val, Almanac.fertilizer_to_water);
+                    val = get_value(val, Almanac.water_to_light);
+                    val = get_value(val, Almanac.light_to_temperature);
+                    val = get_value(val, Almanac.temperature_to_humidity);
+                    val = get_value(val, Almanac.humidity_to_location);
 
-                solution = Math.min(solution, val);
+                    solution = Math.min(solution, val);
+                }
+
             }
             return solution;
         }
 
-        public static Map<Long, Long[]> parse_entries(List<String> chunk){
+        private static Map<Long, Long[]> parse_entries(List<String> chunk){
             var map = new HashMap<Long, Long[]>();
 
             for (int i = 1; i < chunk.size(); i ++){
@@ -76,12 +87,12 @@ public class five {
             return map;
         }
 
-        public static void parse_chunk(List<String> chunk){
+        private static void parse_chunk(List<String> chunk){
             var name = chunk.get(0);
             if (chunk.size() == 1 && name.startsWith("seeds:")){
                 String[] _seeds = name.split(" ");
-                for (int i = 1; i < _seeds.length; i++){
-                    seeds.add(Long.valueOf(_seeds[i]));
+                for (int i = 1; i < _seeds.length; i+=2){
+                    seeds.add(new Long[] {Long.valueOf(_seeds[i]), Long.valueOf(_seeds[i+1])});
                 }
             } else {
                 if (name.equals("seed-to-soil map:")){
