83ed51121655a3fd4117cad244164648ddec0066
AbbiePBC <71944252+AbbiePBC@users.noreply.github.com>
2024-07-26 00:18:38 +0100

    day 8, part 2: rust (relies on input pattern)

--- src/eight.rs
+++ src/eight.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::env;
-
+use num::integer::lcm;
 use itertools::Itertools;
 
 #[derive(Debug)]
@@ -36,47 +36,44 @@ impl Data {
         return Data { steps, map, starting_points };
     }
 
-    fn single_step(&mut self, step: char, idx: usize) -> bool {
-
-        let mut current = &self.starting_points[idx];
-        let options = self.map.get(current).unwrap();
-        current = match step {
-            'L' => &options.0,
-            'R' => &options.1,
-            _ => unreachable!(),
-        };
-        self.starting_points[idx] = current.clone();
-        return current.ends_with("Z")
-    }
-
-    fn multi_traverse(&mut self) -> usize {
-        let mut num_steps_taken: usize = 0;
-        let mut current_step = 0;
+    fn traverse(&self) -> usize {
+        let mut num_steps_taken: usize;
+        let mut current_step: usize;
         let steps: Vec<char> = self.steps.chars().collect_vec();
-        let mut all_complete: bool = false;
-        let num_journeys = self.starting_points.len();
-        while !all_complete {
-
-            all_complete = true;
-            let step = steps[current_step];
-            for i in 0..num_journeys {
-                let complete = self.single_step(step, i);
-                if !complete{
-                    all_complete = false;
-                }
+        let num_journeys: usize = self.starting_points.len();
+        let mut repeat_lens: Vec<usize> = Vec::new();
+        // TODO: there are more efficient ways of doing this, e.g. the loops the other way around
+        // as we then don't have to get the step as often, but it'll do§
+        for i in 0..num_journeys{
+            num_steps_taken = 0;
+            current_step = 0;
+            let mut current = self.starting_points[i].clone();
+            while !current.ends_with("Z") {
+                let step = steps[current_step];
+                let options = self.map.get(&current).unwrap();
+                current = match step {
+                    'L' => options.0.to_string(),
+                    'R' => options.1.to_string(),
+                    _ => unreachable!(),
+                };
+                num_steps_taken += 1;
+                current_step = (current_step + 1) % steps.len();
             }
-
-            num_steps_taken += 1;
-            println!("steps taken = {}", num_steps_taken);
-            current_step = (current_step + 1) % steps.len();
+            repeat_lens.push(num_steps_taken);
         }
 
-        return num_steps_taken;
+        let mut sol = repeat_lens[0];
+        for i in 0..repeat_lens.len() - 1 {
+            sol = lcm(sol, repeat_lens[i+1]);
+        }
+
+        return sol;
     }
+
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut data = Data::new(args.get(1).unwrap().to_string());
-    println!("Solution: {}", data.multi_traverse());
+    let data = Data::new(args.get(1).unwrap().to_string());
+    println!("Solution: {}", data.traverse());
 }
